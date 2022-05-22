e095c76671f5d0fb2fed331906563b8c14f4f8db
Isaiah Bullard <isaiah0812@yahoo.com>
2022-05-21 23:54:00 -0500

    basic photos masonry

--- src/pages/photos/index.tsx
+++ src/pages/photos/index.tsx
@@ -1,10 +1,27 @@
+import { ImageList, ImageListItem } from '@mui/material';
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Image } from 'react-bootstrap';
+import { Divide, Header } from '../../components/styledComponents';
 
 const Photos = () => {
   return (
-    <Container fluid className="page-background">
-      Photos
+    <Container fluid className="page-background" style={{ textAlign: 'center', display: 'flex', alignItems: 'center', flexDirection: 'column', padding: '5em 1em' }}>
+      <Header style={{ transform: 'translate(0px, 10%)' }}>Photos</Header>
+      <Divide width="5%"/>
+      <ImageList variant="masonry" cols={3} gap={8}>
+        <ImageListItem>
+          <img src="/pops.jpg" style={{ width: '50%' }} loading="lazy" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src="/logo192.png" style={{ width: '50%' }} loading="lazy" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src="/logo512.png" style={{ width: '50%' }} loading="lazy" />
+        </ImageListItem>
+        <ImageListItem>
+          <img src="/pops.jpg" style={{ width: '50%' }} loading="lazy" />
+        </ImageListItem>
+      </ImageList>
     </Container>
   )
 }
