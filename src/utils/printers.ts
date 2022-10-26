2109b56dd4413c5a68112ff6f9c4707b515eb995
Isaiah Bullard <isaiah0812@yahoo.com>
2022-10-25 20:08:19 -0600

    use central timezone for all dateTimes

--- src/utils/printers.ts
+++ src/utils/printers.ts
@@ -1,11 +1,11 @@
 export const printDateString = (date: Date) => {
-  return date.toLocaleDateString("en-US", { dateStyle: 'full', timeZone: 'UTC' })
+  return date.toLocaleDateString("en-US", { dateStyle: 'full', timeZone: 'America/Chicago' })
 }
 
 export const printTimeString = (date: Date) => {
-  return date.toLocaleTimeString("en-US", { timeStyle: 'short', timeZone: 'UTC' })
+  return date.toLocaleTimeString("en-US", { timeStyle: 'short', timeZone: 'America/Chicago' })
 }
 
 export const printDateTimeString = (date: Date) => {
-  return date.toLocaleString("en-US", { dateStyle: 'full', timeStyle: 'short', timeZone: 'UTC' }).replace(' at ', ', ')
+  return date.toLocaleString("en-US", { dateStyle: 'full', timeStyle: 'short', timeZone: 'America/Chicago' }).replace(' at ', ', ')
 }
